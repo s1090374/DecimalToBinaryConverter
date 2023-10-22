5f3256008d1bc5714a2f9fba28e9f54169c5f4af
kikok <s1090374@gm.pu.edu.tw>
2023-10-22 17:57:29 +0800

    加入專案檔案。

---
+++ DecimalToBinaryConverter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DecimalToBinaryConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("");
+            int decimalNumber = int.Parse(Console.ReadLine());
+
+            string binaryNumber = DecimalToBinary(decimalNumber);
+
+            Console.WriteLine($"{binaryNumber}");
+        }
+
+        static string DecimalToBinary(int decimalNumber)
+        {
+            if (decimalNumber == 0)
+            {
+                return "0";
+            }
+
+            string binary = "";
+            while (decimalNumber > 0)
+            {
+                int remainder = decimalNumber % 2;
+                binary = remainder + binary;
+                decimalNumber = decimalNumber / 2;
+            }
+
+            return binary;
+        }
+    }
+}
